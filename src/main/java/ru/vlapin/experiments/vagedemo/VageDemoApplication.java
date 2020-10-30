91d85b8056e21d2e96dada97f71050f7c722717f
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2020-10-30 19:01:54 +0300

    initial commit

--- src/main/java/ru/vlapin/experiments/vagedemo/VageDemoApplication.java
+++ src/main/java/ru/vlapin/experiments/vagedemo/VageDemoApplication.java
@@ -1,13 +1,36 @@
 package ru.vlapin.experiments.vagedemo;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VageDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VageDemoApplication.class, args);
-	}
+  @SneakyThrows
+  public static void main(String[] args) {
+    SpringApplication.run(VageDemoApplication.class, args);
 
+//    ABC abc = new ABC(5);
+//    Consumer<ABC> abcConsumer = ABC::kjhdf;
+//    abcConsumer.accept(abc);
+//
+//    ABC.class.getDeclaredMethod("kjhdf").invoke(abc);
+//
+//
+  }
 }
+
+//@RequiredArgsConstructor
+//class ABC {
+//
+//  int x;
+//
+//  @SneakyThrows
+//  public void kjhdf(ABC this) {
+//    System.out.println("true = " + true);;
+//    System.out.println("x = " + this.x);
+//  }
+//
+//
+//}
